9a828b475cff388230a506c0064093563cd35e70
cerenyarol <ceren.yarol@gmail.com>
2022-09-20 18:59:00 +0300

    updates

--- Step2_maximization/Step2_1.py
+++ Step2_maximization/Step2_1.py
@@ -16,16 +16,16 @@ import numpy as np
 if __name__ == '__main__':
     # TODO: defines parameters, two different graph weights
     # initialization of the prices and costs
-    prices = {Products.P1: 7,
-              Products.P2: 4,
-              Products.P3: 5,
+    prices = {Products.P1: 13,
+              Products.P2: 10,
+              Products.P3: 17,
               Products.P4: 3,
-              Products.P5: 2}
-    costs = {Products.P1: 0,
-             Products.P2: 0,
-             Products.P3: 0,
-             Products.P4: 0,
-             Products.P5: 0}
+              Products.P5: 7}
+    costs = {Products.P1: 5,
+             Products.P2: 3,
+             Products.P3: 7,
+             Products.P4: 0.5,
+             Products.P5: 2}
 
     # initialization of the alpha values
     alphas = {Products.P1: 0.2,
@@ -38,26 +38,26 @@ if __name__ == '__main__':
     # initialization of the weights
 
     # initialization of the weights
-    weights = {Products.P1: {Products.P1: 0, Products.P2: 0.8, Products.P3: 0.7, Products.P4: 0.2, Products.P5: 0.3},
-         Products.P2: {Products.P1: 0.65, Products.P2: 0, Products.P3: 0.8, Products.P4: 0.4, Products.P5: 0.2},
-         Products.P3: {Products.P1: 0.7, Products.P2: 0.75, Products.P3: 0, Products.P4: 0.3, Products.P5: 0.4},
-         Products.P4: {Products.P1: 0.3, Products.P2: 0.4, Products.P3: 0.4, Products.P4: 0, Products.P5: 0.8},
-         Products.P5: {Products.P1: 0.4, Products.P2: 0.23, Products.P3: 0.16, Products.P4: 0.85, Products.P5: 0},
+    weights = {Products.P1: {Products.P1: 0.7, Products.P2: 0.67, Products.P3: 0.5, Products.P4: 0.35, Products.P5: 0.55},
+         Products.P2: {Products.P1: 0.69, Products.P2: 0.54, Products.P3: 0.55, Products.P4: 0.59, Products.P5:0.55},
+         Products.P3: {Products.P1: 0.75, Products.P2: 0.5, Products.P3: 0.58, Products.P4: 0.52, Products.P5: 0.64},
+         Products.P4: {Products.P1: 0.7, Products.P2: 0.6, Products.P3: 0.62, Products.P4: 0.53, Products.P5: 0.63},
+         Products.P5: {Products.P1: 0.73, Products.P2: 0.55, Products.P3: 0.65, Products.P4: 0.57, Products.P5: 0.59},
          }
 
     # initialization of the matrix M
-    M = {Products.P1: {Products.P1: 0.7, Products.P2: 1, Products.P3: 0.3, Products.P4: 0.4, Products.P5: 0.2},
-         Products.P2: {Products.P1: 1, Products.P2: 0.5, Products.P3: 0.6, Products.P4: 0.4, Products.P5: 0.1},
-         Products.P3: {Products.P1: 0.8, Products.P2: 0.3, Products.P3: 0, Products.P4: 1, Products.P5: 0.9},
-         Products.P4: {Products.P1: 0.1, Products.P2: 0.1, Products.P3: 1, Products.P4: 0, Products.P5: 0.4},
-         Products.P5: {Products.P1: 0.6, Products.P2: 0.7, Products.P3: 0.8, Products.P4: 1, Products.P5: 0},
+    M = {Products.P1: {Products.P1: 0.3, Products.P2: 0.4, Products.P3: 0.38, Products.P4: 0.45, Products.P5: 0.2},
+         Products.P2: {Products.P1: 0.2, Products.P2: 0.5, Products.P3: 0.34, Products.P4: 0.4, Products.P5: 0.1},
+         Products.P3: {Products.P1: 0.25, Products.P2: 0.31, Products.P3: 0.15, Products.P4: 0.28, Products.P5: 0.44},
+         Products.P4: {Products.P1: 0.19, Products.P2: 0.28, Products.P3: 0.42, Products.P4: 0.17, Products.P5: 0.35},
+         Products.P5: {Products.P1: 0.35, Products.P2: 0.25, Products.P3: 0.29, Products.P4: 0.5, Products.P5: 0.47},
          }
     # initialization of the matrix M0
-    M0 = {Products.P1: {Products.P1: 0.1, Products.P2: 0.05, Products.P3: 0.2, Products.P4: 0.05, Products.P5: 0.05},
-          Products.P2: {Products.P1: 0.2, Products.P2: 0.25, Products.P3: 0.15, Products.P4: 0.05, Products.P5: 0.02},
-          Products.P3: {Products.P1: 0.03, Products.P2: 0.1, Products.P3: 0.07, Products.P4: 0.05, Products.P5: 0.05},
-          Products.P4: {Products.P1: 0.01, Products.P2: 0.01, Products.P3: 0, Products.P4: 0.15, Products.P5: 0.05},
-          Products.P5: {Products.P1: 0.0, Products.P2: 0.0, Products.P3: 0.05, Products.P4: 0.03, Products.P5: 0.02},
+    M0 = {Products.P1: {Products.P1: 0.01, Products.P2: 0.05, Products.P3: 0.02, Products.P4: 0.03, Products.P5: 0.025},
+          Products.P2: {Products.P1: 0.02, Products.P2: 0.025, Products.P3: 0.015, Products.P4: 0.05, Products.P5: 0.02},
+          Products.P3: {Products.P1: 0.03, Products.P2: 0.01, Products.P3: 0.04, Products.P4: 0.05, Products.P5: 0.038},
+          Products.P4: {Products.P1: 0.01, Products.P2: 0.017, Products.P3: 0.02, Products.P4: 0.015, Products.P5: 0.05},
+          Products.P5: {Products.P1: 0.03, Products.P2: 0.02, Products.P3: 0.05, Products.P4: 0.035, Products.P5: 0.01},
           }
     # initialization of the 5 fixed webpages
     pages = {Products.P1: [Products.P1, Products.P2, Products.P3],
@@ -69,39 +69,39 @@ if __name__ == '__main__':
     # initialization of the weight for each class defined by discounted product
     returnerWeights = {}
     returnerWeights[Products.P1] = {
-        Products.P1: {Products.P1:0, Products.P2: 0.8, Products.P3: 0.5, Products.P4: 1, Products.P5: 1},
-        Products.P2: {Products.P1: 1, Products.P2: 0, Products.P3: 1, Products.P4: 1, Products.P5: 1},
-        Products.P3: {Products.P1: 1, Products.P2: 1, Products.P3: 0, Products.P4: 1, Products.P5: 1},
-        Products.P4: {Products.P1: 1, Products.P2: 1, Products.P3: 1, Products.P4: 0, Products.P5: 1},
-        Products.P5: {Products.P1: 1, Products.P2: 1, Products.P3: 1, Products.P4: 1, Products.P5: 0},
+        Products.P1: {Products.P1:0, Products.P2: 0.65, Products.P3: 0.5, Products.P4: 0.4, Products.P5: 0.25},
+        Products.P2: {Products.P1: 0.6, Products.P2: 0, Products.P3: 0.35, Products.P4: 0.2, Products.P5: 0.5},
+        Products.P3: {Products.P1: 0.48, Products.P2: 0.50, Products.P3: 0, Products.P4: 0.3, Products.P5: 0.4},
+        Products.P4: {Products.P1: 0.49, Products.P2: 0.6, Products.P3: 0.45, Products.P4: 0, Products.P5: 0.55},
+        Products.P5: {Products.P1: 0.35, Products.P2: 0.45, Products.P3: 0.55, Products.P4: 0.65, Products.P5: 0},
     }
     returnerWeights[Products.P2] = {
-        Products.P1: {Products.P1: 0, Products.P2: 1, Products.P3: 1, Products.P4: 1, Products.P5: 1},
-        Products.P2: {Products.P1: 1, Products.P2: 0, Products.P3: 1, Products.P4: 1, Products.P5: 1},
-        Products.P3: {Products.P1: 1, Products.P2: 1, Products.P3: 0, Products.P4: 1, Products.P5: 1},
-        Products.P4: {Products.P1: 1, Products.P2: 1, Products.P3: 1, Products.P4: 0, Products.P5: 1},
-        Products.P5: {Products.P1: 1, Products.P2: 1, Products.P3: 1, Products.P4: 1, Products.P5: 0},
+        Products.P1: {Products.P1: 0, Products.P2: 0.45, Products.P3: 0.48, Products.P4: 0.35, Products.P5: 0.5},
+        Products.P2: {Products.P1: 0.55, Products.P2: 0, Products.P3: 0.45, Products.P4: 0.63, Products.P5: 0.38},
+        Products.P3: {Products.P1: 0.59, Products.P2: 0.47, Products.P3: 0, Products.P4: 0.58, Products.P5: 0.61},
+        Products.P4: {Products.P1: 0.41, Products.P2: 0.45, Products.P3: 0.6, Products.P4: 0, Products.P5: 0.5},
+        Products.P5: {Products.P1: 0.45, Products.P2: 0.39, Products.P3: 0.47, Products.P4: 0.55, Products.P5: 0},
     }
     returnerWeights[Products.P3] = {
-        Products.P1: {Products.P1: 0, Products.P2: 1, Products.P3: 1, Products.P4: 1, Products.P5: 1},
-        Products.P2: {Products.P1: 1, Products.P2: 0, Products.P3: 1, Products.P4: 1, Products.P5: 1},
-        Products.P3: {Products.P1: 1, Products.P2: 1, Products.P3: 0, Products.P4: 1, Products.P5: 1},
-        Products.P4: {Products.P1: 1, Products.P2: 1, Products.P3: 1, Products.P4: 0, Products.P5: 1},
-        Products.P5: {Products.P1: 1, Products.P2: 1, Products.P3: 1, Products.P4: 1, Products.P5: 0},
+        Products.P1: {Products.P1: 0, Products.P2: 0.43, Products.P3: 0.58, Products.P4: 0.55, Products.P5:0.39},
+        Products.P2: {Products.P1: 0.42, Products.P2: 0, Products.P3: 0.57, Products.P4: 0.61, Products.P5: 0.6},
+        Products.P3: {Products.P1: 0.47, Products.P2: 0.53, Products.P3: 0, Products.P4: 0.58, Products.P5: 0.62},
+        Products.P4: {Products.P1: 0.41, Products.P2: 0.49, Products.P3: 0.58, Products.P4: 0, Products.P5: 0.6},
+        Products.P5: {Products.P1: 0.45, Products.P2: 0.5, Products.P3: 0.35, Products.P4: 0.6, Products.P5: 0},
     }
     returnerWeights[Products.P4] = {
-        Products.P1: {Products.P1: 0, Products.P2: 1, Products.P3: 1, Products.P4: 1, Products.P5: 1},
-        Products.P2: {Products.P1: 1, Products.P2: 0, Products.P3: 1, Products.P4: 1, Products.P5: 1},
-        Products.P3: {Products.P1: 1, Products.P2: 1, Products.P3: 0, Products.P4: 1, Products.P5: 1},
-        Products.P4: {Products.P1: 1, Products.P2: 1, Products.P3: 1, Products.P4: 0, Products.P5: 1},
-        Products.P5: {Products.P1: 1, Products.P2: 1, Products.P3: 1, Products.P4: 1, Products.P5: 0},
+        Products.P1: {Products.P1: 0, Products.P2: 0.5, Products.P3: 0.4, Products.P4: 0.37, Products.P5: 0.61},
+        Products.P2: {Products.P1: 0.39, Products.P2: 0, Products.P3: 0.47, Products.P4: 0.43, Products.P5: 0.53},
+        Products.P3: {Products.P1: 0.65, Products.P2: 0.55, Products.P3: 0, Products.P4: 0.47, Products.P5: 0.6},
+        Products.P4: {Products.P1: 0.5, Products.P2: 0.37, Products.P3: 0.47, Products.P4: 0, Products.P5: 0.44},
+        Products.P5: {Products.P1: 0.51, Products.P2: 0.57, Products.P3: 0.60, Products.P4: 0.63, Products.P5: 0},
     }
     returnerWeights[Products.P5] = {
-        Products.P1: {Products.P1: 0, Products.P2: 1, Products.P3: 1, Products.P4: 1, Products.P5: 1},
-        Products.P2: {Products.P1: 1, Products.P2: 0, Products.P3: 1, Products.P4: 1, Products.P5: 1},
-        Products.P3: {Products.P1: 1, Products.P2: 1, Products.P3: 0, Products.P4: 1, Products.P5: 1},
-        Products.P4: {Products.P1: 1, Products.P2: 1, Products.P3: 1, Products.P4: 0, Products.P5: 1},
-        Products.P5: {Products.P1: 1, Products.P2: 1, Products.P3: 1, Products.P4: 1, Products.P5: 0},
+        Products.P1: {Products.P1: 0, Products.P2: 0.45, Products.P3: 0.42, Products.P4: 0.37, Products.P5: 0.58},
+        Products.P2: {Products.P1: 0.57, Products.P2: 0, Products.P3: 0.5, Products.P4: 0.6, Products.P5: 0.57},
+        Products.P3: {Products.P1: 0.38, Products.P2: 0.45, Products.P3: 0, Products.P4: 0.47, Products.P5: 0.50},
+        Products.P4: {Products.P1: 0.52, Products.P2: 0.39, Products.P3: 0.49, Products.P4: 0, Products.P5: 0.5},
+        Products.P5: {Products.P1: 0.65, Products.P2: 0.54, Products.P3: 0.45, Products.P4: 0.59, Products.P5: 0},
     }
     theta=0.8
     # initialization of the environment
